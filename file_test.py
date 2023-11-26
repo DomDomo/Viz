77eeeb4da926b67e220d3026ea56857096972a9e
DomDomo <g.domis02@gmail.com>
2023-11-26 03:09:39 +0000

    Add stable bar chart and map

--- file_test.py
+++ file_test.py
@@ -34,14 +34,9 @@ for column in source.columns:
 
 indicators = list(source.columns)[1:]
 
-# life_satisfaction = 'Life satisfaction (average score)'
-# life_satisfaction = "Personal earnings ($)"
-life_satisfaction = "Household net wealth ($)"
-
-
 indicator_dropdown = alt.binding_select(options=indicators, name="Indicator: ")
 
-indicator_param = alt.param(value=life_satisfaction, bind=indicator_dropdown)
+indicator_param = alt.param(value=indicators[0], bind=indicator_dropdown)
 
 click = alt.selection_point(encodings=['x'])
 
@@ -57,8 +52,6 @@ countries_shape = gpd.read_file(url)  # zipped shapefile
 countries_shape = countries_shape[['NAME', 'CONTINENT', 'ISO_A3', 'geometry']]
 
 countries_shape = countries_shape.rename(columns={'NAME': 'Country'})
-countries_shape['Country'] = countries_shape['Country'].replace(
-    'United States of America', 'United States')
 
 # Data generators for the background
 sphere = alt.sphere()
@@ -76,32 +69,7 @@ background = (
 
 click_countries = alt.selection_multi(fields=["Country"])
 
-new_chart = alt.Chart(source).mark_bar().encode(
-    x=alt.X('Country:N', sort='-y', axis=alt.Axis(labelAngle=-45), title=''),
-    y=alt.Y('y:Q', title=''),
-    color=alt.Color('y:Q', scale=alt.Scale(
-        scheme='yellowgreenblue'), legend=None),
-    opacity=alt.condition(click_countries, alt.value(1), alt.value(0.2)),
-    tooltip='tooltip:N',
-).properties(
-    width=750,
-    height=450
-).transform_calculate(
-    y=f'datum[{indicator_param.name}]'  # Variable Y axis
-).transform_calculate(
-    tooltip='datum.Country + ": " + format(datum.y, ",")'
-).transform_filter(
-    'isValid(datum.y)'  # Removes NaN values
-).add_params(
-    indicator_param  # Add dropdown parameter
-).add_params(
-    click_countries  # Add dropdown parameter
-)
-
-source_melted = source.melt(
-    id_vars='Country', var_name='Indicator', value_name='Value')
-source_melted = source_melted.dropna(subset=['Value'])
-print(source_melted)
+life_satisfaction = 'Life satisfaction (average score)'
 
 choropleth = (
     alt.Chart(countries_shape)
@@ -119,7 +87,7 @@ choropleth = (
             gradientLength=350)),
         opacity=alt.condition(click_countries, alt.value(1), alt.value(0.2)),
         tooltip=[
-            alt.Tooltip('Country:N', title='Country'),
+            alt.Tooltip('NAME:N', title='Country'),
             alt.Tooltip(f'{life_satisfaction}:Q'),
         ]
     )
@@ -128,72 +96,32 @@ choropleth = (
     )
 )
 
-choropleth = (
-    alt.Chart(countries_shape)
-    .mark_geoshape()
-    .transform_lookup(
-        lookup='Country',
-        from_=alt.LookupData(data=source_melted, key='Country',
-                             fields=['Value'])
-    )
-    .transform_filter(
-        # Filter data based on the selected indicator
-        (alt.datum.Indicator == indicator_param)
-    )
-    .encode(
-        color=alt.Color('Value:Q', legend=alt.Legend(
-            orient='none',
-            legendX=180, legendY=380,
-            direction='horizontal',
-            gradientLength=350)),
-        opacity=alt.condition(click_countries, alt.value(1), alt.value(0.2)),
-        tooltip=[
-            alt.Tooltip('Country:N', title='Country'),
-            alt.Tooltip('Value:Q', title=indicator_param.name),
-        ]
-    )
-    .add_params(
-        click_countries
-    )
-)
+map = background + choropleth
 
-# map = background + choropleth
 
-# new_map = (basemap + map).project(
-#     "equalEarth"
-# ).properties(
-#     width=850,
-#     height=450
-# ).resolve_scale(
-#     color='independent'
-# )
-
-# # mcv = new_chart | new_map
-# mcv = new_map & new_chart
-
-# mcv.save('JustChart.html')
-
-# Assuming 'df' is your DataFrame
-df = source_melted
-
-# Create a dropdown selection menu for the Indicator
-indicator_dropdown = alt.binding_select(
-    options=df['Indicator'].unique().tolist())
-indicator_select = alt.selection_single(
-    fields=['Indicator'], bind=indicator_dropdown, name='Select')
-
-# Create the bar chart
-chart = alt.Chart(df).mark_bar().encode(
-    x='Country:N',
-    y='Value:Q',
-    color='Country:N',
-    tooltip=['Country', 'Indicator', 'Value']
-).add_selection(
-    indicator_select
+new_chart = alt.Chart(source).mark_bar().encode(
+    x=alt.X('Country:N', sort='-y', axis=alt.Axis(labelAngle=-45), title=''),
+    y=alt.Y('y:Q', title=''),
+    color=alt.Color('y:Q', scale=alt.Scale(
+        scheme='yellowgreenblue'), legend=None),
+    opacity=alt.condition(click_countries, alt.value(1), alt.value(0.2)),
+    tooltip='tooltip:N',
+).properties(
+    width=800,
+    height=400
+).transform_calculate(
+    y=f'datum[{indicator_param.name}]'  # Variable Y axis
+).transform_calculate(
+    tooltip='datum.Country + ": " + format(datum.y, ",")'
 ).transform_filter(
-    indicator_select
+    'isValid(datum.y)'  # Removes NaN values
+).add_params(
+    indicator_param  # Add dropdown parameter
+).add_params(
+    click_countries  # Add dropdown parameter
 )
 
-chart
+color_map = new_chart | (
+    basemap + map).project("equalEarth").properties(width=700, height=400)
 
-chart.save('JustChart.html')
+color_map.save('JustChart.html')
--- new_dropdown.py
+++ new_dropdown.py
@@ -45,16 +45,17 @@ source = source.dropna(subset=['Value'])
 
 life_satisfaction = "Life satisfaction (average score)"
 
+indicators = source['Indicator'].unique().tolist()
+
 # Create a dropdown selection menu for the Indicator
 indicator_dropdown = alt.binding_select(
-    options=source['Indicator'].unique().tolist(),
+    options=indicators,
     name="Indicator: "
 )
 
 indicator_select = alt.selection_point(
     fields=['Indicator'],
     bind=indicator_dropdown,
-    name='Select',
     value=life_satisfaction
 )
 
@@ -71,8 +72,9 @@ chart = alt.Chart(source).mark_bar().encode(
     tooltip=[alt.Tooltip('Country:N'), alt.Tooltip(
         'Indicator:N'), alt.Tooltip('Value:Q', format=',')],
 ).properties(
+    title="Bar Chart of OECD Indicators by Country",
     width=750,
-    height=300,
+    height=400,
     # padding={"left": 35, "top": 5, "right": 5, "bottom": 5}
 ).transform_filter(
     indicator_select
@@ -108,6 +110,15 @@ background = (
     .mark_geoshape(fill='#D3D3D3')
 )
 
+legend = alt.Legend(
+    title=life_satisfaction,
+    orient='none',
+    legendX=200, legendY=380,
+    direction='horizontal',
+    gradientLength=400,
+    values=list(range(5, 9))
+)
+
 # Filter the source data to include only the rows for the 'Life satisfaction' indicator
 source_filtered = source[source['Indicator'] == life_satisfaction]
 
@@ -117,18 +128,16 @@ choropleth = (
     .transform_lookup(
         lookup='Country',
         from_=alt.LookupData(data=source_filtered, key='Country',
-                             fields=['Value'])
+                             fields=['Value',
+                                     'Indicator'])
     )
     .encode(
-        color=alt.Color('Value:Q', legend=alt.Legend(
-            orient='none',
-            legendX=180, legendY=380,
-            direction='horizontal',
-            gradientLength=350)),
+        color=alt.Color('Value:Q', legend=legend),
         opacity=alt.condition(click_countries, alt.value(1), alt.value(0.2)),
         tooltip=[
             alt.Tooltip('Country:N', title='Country'),
-            alt.Tooltip('Value:Q', title='Life satisfaction'),
+            alt.Tooltip('Indicator:N', title="Indicator"),
+            alt.Tooltip('Value:Q', title="Value"),
         ]
     )
     .add_params(
@@ -141,12 +150,56 @@ map = background + choropleth
 new_map = (basemap + map).project(
     "equalEarth"
 ).properties(
+    title="Interactive Global Map of Life Satisfaction",
     width=850,
     height=450
 ).resolve_scale(
     color='independent'
 )
 
-mcv = new_map | chart
+mcv = chart | new_map
+
+
+brush = alt.selection_interval(resolve='global')
+
+life_satisfaction = 'Life satisfaction (average score)'
+
+base = alt.Chart(source).mark_circle(size=100).encode(
+    y=life_satisfaction,
+    color=alt.condition(brush, 'Origin', alt.ColorValue('gray'), legend=None)
+).add_params(
+    brush
+).properties(
+    width=250,
+    height=250
+)
+
+charts = []
+
+for i in range(len(indicators)):
+    # sub = source.dropna(subset = [indicators[num], life_satisfaction], inplace=False)
+
+    # # Calculate the distance of each point from the origin (0,0), adding a small constant to avoid taking the square root of 0
+    # sub['distance'] = (source[life_satisfaction])**2 + (source[indicators[i]])**2
+
+    chart1 = base.transform_filter(
+        (alt.datum[indicators[i]] != 0) & (alt.datum[life_satisfaction] != 0)
+    ).encode(
+        x=indicators[i],
+        # color=alt.Color('distance:Q', scale=alt.Scale(scheme='viridis'), legend=None),
+        # color=alt.Color(f'{life_satisfaction}:Q', scale=alt.Scale(scheme='blues'), legend=None),
+        tooltip=['Country:N', f'{life_satisfaction}:Q', f'{indicators[i]}:Q']
+    )
+
+    charts.append(chart1)
+
+
+alt.vconcat(
+    alt.hconcat(*charts[:4]),
+    alt.hconcat(*charts[4:8]),
+    alt.hconcat(*charts[8:12]),
+    alt.hconcat(*charts[12:]),
+    title='Index of Multiple Deprivation Dashboard'
+)
 
 mcv.save('JustChart.html')
